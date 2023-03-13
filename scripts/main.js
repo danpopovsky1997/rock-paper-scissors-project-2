cec7ef9b6f5bbee02aca0bfd91faa4026038fc8b
danpopovsky1997 <danielpopovsky1010@gmail.com>
2023-03-13 18:01:14 +0000

    Create function to get game winner

--- scripts/main.js
+++ scripts/main.js
@@ -19,3 +19,40 @@ function play(e) {
     const winner = getWinner(playerChoice, computerChoice);
     showWinner(winner, computerChoice);
 }
+
+// Get Computer Choice
+function getComputerChoice() {
+    const rand = Math.random();
+    if (rand < 0.34) {
+        return 'rock';
+    } else if (rand <= 0.67) {
+        return 'paper';
+    } else {
+        return 'scissors'
+    }
+}
+
+// Get game winner
+function getWinner(p, c) {
+    if (p === c) {
+        return 'draw';
+    } else if (p === 'rock') {
+        if (c === 'paper') {
+            return 'computer';
+        } else {
+            return 'player';
+        }
+    } else if (p === 'paper') {
+        if (c === 'scissors') {
+            return 'computer';
+        } else {
+            return 'player';
+        }
+    } else if (p === 'scissors') {
+        if (c === 'rock') {
+            return 'computer';
+        } else {
+            return 'player';
+        }
+    }
+}
